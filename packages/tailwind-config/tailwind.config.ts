a456ae1f05d82b952a125bb53bc8b8ae54e95f43
ManabBala <manabbalaofficial@gmail.com>
2024-10-24 16:49:22 +0530

    modified global tailwind config and devpanel's css

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -2,7 +2,18 @@ import type { Config } from 'tailwindcss/types/config';
 
 export default {
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      borderWidth: {
+        '1': '1px',
+      },
+    },
+
+    container: {
+      center: true,
+    },
   },
   plugins: [],
 } as Omit<Config, 'content'>;
